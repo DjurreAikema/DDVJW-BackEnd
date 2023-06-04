8456a06632e0c2a4fd937d51f802befb196f18db
Djurre <djurre10@hotmail.com>
2023-06-04 14:54:46 +0200

    Users

--- core/serializers.py
+++ core/serializers.py
@@ -7,3 +7,21 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        # Extract the password
+        password = validated_data.pop('password', None)
+        # Create the user
+        instance = self.Meta.model(**validated_data)
+
+        # Hash the password and set in on the user
+        if password is not None:
+            instance.set_password(password)
+
+        # Save the user
+        instance.save()
+
+        return instance
