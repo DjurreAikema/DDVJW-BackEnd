49d6b17cfef8648be7645f7d42b8dee99d11fa34
Djurre <djurre10@hotmail.com>
2023-06-04 15:00:39 +0200

    Users

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 
-from core.views import RegisterAPIView
+from core.views import RegisterAPIView, LoginAPIView
 
 urlpatterns = [
     path('register', RegisterAPIView.as_view()),
+    path('login', LoginAPIView.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from core.models import User
 from core.serializers import UserSerializer
 
 
@@ -16,4 +17,21 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
+        return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+
+        serializer = UserSerializer(user)
         return Response(serializer.data)
