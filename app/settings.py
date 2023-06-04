fdca9e5fd48953aa667d3afb5861511f67af627a
Djurre <djurre10@hotmail.com>
2023-06-04 14:22:37 +0200

    DB

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'auth'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ddvjw',
+        'USER': 'root',
+        'PASSWORD': '117512',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
