b452d6b38c17f27eb4862f25107b572ecc7cc446
Djurre <djurre10@hotmail.com>
2023-06-05 11:16:15 +0200

    Users

--- core/models.py
+++ core/models.py
@@ -22,4 +22,4 @@ class UserToken(models.Model):
 
 class ResetPassword(models.Model):
     email = models.CharField(max_length=255)
-    token = models.CharField(max_length=20, unique=True)
+    token = models.CharField(max_length=255, unique=True)
