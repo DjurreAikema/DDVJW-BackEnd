5bb22502c3b99cfc299bc33c313fa9cd07f9a034
DjurreAikema <djurreaikema@gmail.com>
2023-07-09 14:47:48 +0200

    SchoolId

--- schools/urls.py
+++ schools/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from schools import views
 
 urlpatterns = [
-    path('/', views.SchoolList.as_view()),
-    path('/<int:pk>/', views.SchoolDetail.as_view()),
+    path('', views.SchoolList.as_view()),
+    path('<int:pk>/', views.SchoolDetail.as_view()),
 ]
