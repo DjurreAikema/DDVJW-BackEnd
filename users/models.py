35f7416afe08819d6a7737de3f7af85e92ea32d1
Djurre <djurre10@hotmail.com>
2023-07-10 13:39:19 +0200

    reset

---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('trainer', 'Trainer'),
+        ('client', 'Client')
+    )
+
+    username = None
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=7, choices=ROLE_CHOICES, blank=False, null=False, default='client')
+    # school = models.ForeignKey(School, on_delete=models.CASCADE, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
