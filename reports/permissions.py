59b7758aa9d848202879f389e93a4f64c50ee2d2
Djurre <djurre10@hotmail.com>
2023-07-10 11:24:05 +0200

    Reports

---
+++ reports/permissions.py
@@ -0,0 +1,47 @@
+from rest_framework.permissions import BasePermission
+
+
+class CanViewReportList(BasePermission):
+    def has_permission(self, request, view):
+        # Allow admins to view all reports, trainers to view their own reports, and clients to view their assigned reports
+        if request.user.role == 'admin':
+            return True
+        elif request.user.role == 'trainer' and view.action == 'list':
+            return True
+        elif request.user.role == 'client' and view.action == 'list':
+            return True
+        return False
+
+
+class CanViewReportDetails(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Allow admins to view all reports, trainers to view their own reports, and clients to view their assigned reports
+        if request.user.role == 'admin':
+            return True
+        elif request.user.role == 'trainer' and obj.trainer == request.user:
+            return True
+        elif request.user.role == 'client' and obj.client == request.user:
+            return True
+        return False
+
+
+class CanUpdateReport(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Only allow trainers, admins, and the assigned client to update reports
+        if request.user.role == 'admin':
+            return True
+        elif request.user.role == 'trainer':
+            return True
+        elif request.user.role == 'client' and view.action in ['partial_update']:
+            return True
+        return False
+
+
+class CanDeleteReport(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Only allow trainers to delete reports they made, and admins to delete all reports
+        if request.user.role == 'admin':
+            return True
+        elif request.user.role == 'trainer' and obj.trainer == request.user:
+            return True
+        return False
