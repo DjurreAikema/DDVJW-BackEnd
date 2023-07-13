5c812e9f6fd15164a5d6a5b4117fd78293ee1753
Djurre <djurre10@hotmail.com>
2023-07-13 10:37:46 +0200

    school on user

--- core/serializers.py
+++ core/serializers.py
@@ -1,15 +1,25 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from .models import User
 
 
 # Serializers convert Django models into another format. In this case we convert a Django model to json.
 class UserSerializer(ModelSerializer):
+    school_name = serializers.CharField(max_length=255, allow_null=True)
+
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'school', 'role']
+        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'school', 'school_name', 'role']
         extra_kwargs = {
             'password': {'write_only': True}
         }
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if representation['school']:
+            representation['school_name'] = instance.school.name
+
+        return representation
 
     def create(self, validated_data):
         # Extract the password
